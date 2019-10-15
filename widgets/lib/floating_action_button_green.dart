e32b6d6eab79559c4ac710ab716ddba28ea8627a
cristian alvarado esperano <Cris_ales@LIVE.COM.MX>
2019-10-15 17:00:58 -0600

    Se añadio el boton para agregar la imagen a favoritos.

---
+++ widgets/lib/floating_action_button_green.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class FloatingActionButtonGreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _FloatingActionButtonGreen();
+  }
+
+}
+
+class _FloatingActionButtonGreen extends State<FloatingActionButtonGreen>{
+  bool isFavorite = true;
+  IconData buttonIcon = Icons.favorite_border;
+  String mensaje;
+  void onPressedFav(){
+    if (!isFavorite){
+      mensaje = "Agregado a favoritos";
+      buttonIcon = Icons.favorite;
+    }else{
+      mensaje = "Eliminado de favoritos";
+      buttonIcon = Icons.favorite_border;
+    }
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+
+    Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(mensaje),
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return FloatingActionButton(
+      backgroundColor: Color(0xFF11DA53),
+      mini: true,
+      tooltip: "Fav",
+      onPressed: onPressedFav,
+      child: Icon(
+          buttonIcon
+      ),
+    );
+  }
+
+}
